85ce01f0fdffd8a4380fd6cb0b60ffa2291c3bd2
user-faiz <email2faizm@gmail.com>
2023-07-19 03:37:19 +0530

    update index.js

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 function component() {
     const element = document.createElement('div');
   
-    // Lodash, currently included via a script, is required for this line to work
+    // Lodash, imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
     return element;
